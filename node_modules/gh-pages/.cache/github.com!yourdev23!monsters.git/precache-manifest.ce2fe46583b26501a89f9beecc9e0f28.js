e298a3eae77c63a23ed28e682dbc05ca0bed73e3
Sunbir Ghag <Dev@Sunbirs-MacBook-Pro.local>
2019-10-30 09:09:44 -0700

    Updates

---
+++ node_modules/gh-pages/.cache/github.com!yourdev23!monsters.git/precache-manifest.ce2fe46583b26501a89f9beecc9e0f28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3400b4447625692a73f6312fed2a760f",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "65fe23b67fb2c686b465",
+    "url": "/monsters/static/css/main.c1d2dbee.chunk.css"
+  },
+  {
+    "revision": "849a0e77c7a65ebc01db",
+    "url": "/monsters/static/js/2.64cdb3b7.chunk.js"
+  },
+  {
+    "revision": "65fe23b67fb2c686b465",
+    "url": "/monsters/static/js/main.5def0ffa.chunk.js"
+  },
+  {
+    "revision": "a780c5024727f4e9d1d4",
+    "url": "/monsters/static/js/runtime-main.4488f325.js"
+  }
+]);
